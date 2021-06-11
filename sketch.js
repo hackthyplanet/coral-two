6834de738e42a1fa534e1fcb40a5826aa17b3770
Fish Girl <81677095+hackthyplanet@users.noreply.github.com>
2021-06-11 16:04:57 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,21 @@
+//////////////////CODE TWO/////////////////////////////////////////////////////////////////////////
 
-const flock = []; // Array of boids
-let depth = 800; // The Z location of the boid tend to stay between +depth/2 and -depth/2
-let gap = 300; // Boids can go further than the edges, this further distance is the gap
-let quadTree; // A quad tree to minimize the cost of distance calculation
-let unitX, unitY, unitZ; // Unit vectors pointing in the X, Y, and Z directions
+const flock = [];
+let depth = 800; 
+let gap = 300; 
+let quadTree;
+let unitX, unitY, unitZ; 
 
-let useQuadTree = true; // Toogle the use of a quad tree
-let showPerceptionRadius = false; // Toogle vizualization of perception radius
-let goMiddle = false; // Pressing "a" toogle it, making all boids go to the center
-let downloadCanvas = false; // Make it true to lower frameRate and download the canvas each 2 frames
+let useQuadTree = true; 
+let showPerceptionRadius = false; 
+let goMiddle = false; 
+let downloadCanvas = false; 
 
 let boid
-//let boidsSlider, perceptionSlider, alignmentSlider, separationSlider; // Sliders
-let boidsP, perceptionP, alignmentP, cohesionP, separationP; // Paragraphs
-let startingBoids = 100; // Amount of boid at the start of the sketch
-let startingPerception = 90; // Perception radius at the start of the sketch
-let t = 0; // Counts the frame from the time boids go out of the middle of space
-
-
-let numbernumberone
-let numbernumberminusone
+let boidsP, perceptionP, alignmentP, cohesionP, separationP; 
+let startingBoids = 100; 
+let startingPerception = 90;
+let t = 0; 
 
 
 let music 
@@ -33,11 +29,8 @@ function preload(){
 
 }
 function setup (){
-	
-    
-	music.play()
-
-	
+	    
+	music.loop()
 
 	let p5Canvas;
 	if (downloadCanvas) {
@@ -57,13 +50,11 @@ function setup (){
   let cameraZ = -280 / 500 * depth;
   camera(cameraX, cameraY, cameraZ, 0, 0, 0, 0, 0, 1);
   
-  // Create the DOM elements: sliders and paragraphs
   createDOMs();
 
-  // Create an initial population of 100 boids
+
   for (let i = 0; i < 200; i++) {
     pushRandomBoid();
-
 
 	noCursor()
   }
@@ -71,8 +62,7 @@ function setup (){
 
 // DRAW FUNCTION ---------------------------------------------------
 function draw() {
-  // Background and lightning
-  //background(0, 22.0, 84.3);
+  
 
   background(0)
   let dirX = (mouseX / width - 0.5) * 2;
@@ -101,98 +91,64 @@ function draw() {
  
 }
 
-function tempsoundting (){
-
-	
-	let spectrum = fft.analyze();
-	noStroke();
-	fill(255, 0, 255);
-	translate(250, -200, 150)
-	for (let i = 0; i< spectrum.length; i++){
-	  let x = map(i, 0, spectrum.length, 0, width);
-	  let h = -height + map(spectrum[i], 0, 255, height, 0);
-	  sphere(x, height, width / spectrum.length, h )
-	}
-  
-	let waveform = fft.waveform();
-	noFill();
-	beginShape();
-	stroke(20);
-	for (let i = 0; i < waveform.length; i++){
-	  let x = map(i, 0, waveform.length, 0, width);
-	  let y = map( waveform[i], -1, 1, 0, height);
-	  vertex(x,y);
-	}
-	endShape();
-}
-
 
   function boidfam (){
-  // Make the quad tree
-  //translate(-windowWidth, -windowHeight, -windowWidth, -windowHeight)
+
   let boundary = new Cube(0, 0, 0, width + 4 * gap, height + 4 * gap, depth + 4 * gap);
   quadTree = new QuadTree(boundary, 4);
   for (let boid of flock) {
     quadTree.insert(boid);
   }
-  
-  // Each boid determines its acceleration for the next frame
+
+	  
   for (let boid of flock) {
     boid.flock(flock, quadTree);
   }
-  // Each boid updates its position and velocity, and is displayed on screen
+
+	  
   for (let boid of flock) {
     boid.update(gap);
     boid.show();
   }
 
-  // Adjust the amount of boids on screen according to the slider value
   let maxBoids = 200;
   let difference = flock.length - maxBoids;
   if (difference < 0) {
     for (let i = 0; i < -difference; i++) {
-      pushRandomBoid(); // Add boids if there are less boids than the slider value
+      pushRandomBoid(); 
     }
   } else if (difference > 0) {
     for (let i = 0; i < difference; i++) {
-      flock.pop(); // Remove boids if there are more boids than the slider value
+      flock.pop(); 
     }
   }
   
-  t++; // t counts the number of frames, it is used to not have cohesion in the first 40 frames
-  // Download the current state of the canvas as .png
+  t++; 
+
   if (downloadCanvas && frameCount % 2 == 1) download(canvas, 'Flocking_' + frameCount + '.png');
 }
 
-// Create the DOM elements
+
 function createDOMs() {
  
 }
 
-// Make a new boid
+
 function pushRandomBoid() {
-  //let pos = createVector(random(width), random(height), random(-depth/2, depth/2)); // Uncomment and comment next line to create boids at random position
-  let pos = createVector(0, 0, 0); // Create a boid at the center of space
-  let vel = p5.Vector.random3D().mult(random(0.5, 3)); // Give a random velocity
-  let boid = new Boid(pos, vel); // Create a new boid
-  flock.push(boid); // Add the new boid to the flock
+  let pos = createVector(0, 0, 0); 
+  let vel = p5.Vector.random3D().mult(random(0.5, 3)); 
+  let boid = new Boid(pos, vel); 
+  flock.push(boid); 
 }
 
-// Canvas Donwload to make the gif
-// Taken from Jose Quintana: https://codepen.io/joseluisq/pen/mnkLu
-function download(canvas, filename) {
-  /// create an "off-screen" anchor tag
-  var lnk = document.createElement('a'), e;
 
-  /// the key here is to set the download attribute of the a tag
+function download(canvas, filename) {
+ 
+  var lnk = document.createElement('a'), e;
   lnk.download = filename;
 
-  /// convert canvas content to data-uri for link. When download
-  /// attribute is set the content pointed to by link will be
-  /// pushed as "download" in HTML5 capable browsers
   lnk.href = canvas.toDataURL("image/png;base64");
-
-  /// create a "fake" click-event to trigger the download
+	
   if (document.createEvent) {
     e = document.createEvent("MouseEvents");
     e.initMouseEvent("click", true, true, window,
@@ -205,35 +161,32 @@ function download(canvas, filename) {
   }
 }
 
-// Boid class with flocking behavior
 class Boid {
 	constructor(pos, vel) {
-	  this.pos = pos; // Position
-	  this.vel = vel; // Velocity
-	  this.acc = createVector(0, 0, 0); // Acceleration
-	  this.maxForce = 0.1; // Maximum steering force for alignment, cohesion, separation
-	  this.maxSpeed = 5; // Desired velocity for the steering behaviors
-	  this.r = (0); // red color of the boid
-	  this.g = floor(random(150, 200)); // green color of the boid
-	  this.b = floor(random(50, 120)); // blue color of the boid
+	  this.pos = pos; 
+	  this.vel = vel; 
+	  this.acc = createVector(0, 0, 0); 
+	  this.maxForce = 0.1; 
+	  this.maxSpeed = 5; 
+	  this.r = (0); 
+	  this.g = floor(random(150, 200)); 
+	  this.b = floor(random(50, 120)); 
 	}
 	
-	// Alignment rule
-	// Steering to average neighbors velocity
 	alignment(neighbors) {
 	  let steering = createVector();
-	  for (let other of neighbors) steering.add(other.vel); // Sum of neighbor velocities 
+	  for (let other of neighbors) steering.add(other.vel); 
 	  if (neighbors.length > 0) {
-		steering.div(neighbors.length); // Average neighbors velocity
-		steering.setMag(this.maxSpeed); // Desired velocity
-		steering.sub(this.vel); // Actual steering
-		steering.limit(this.maxForce); // Steering limited to maxForce
+		steering.div(neighbors.length); 
+		steering.setMag(this.maxSpeed); 
+		steering.sub(this.vel); 
+		steering.limit(this.maxForce); 
 	  }
 		return steering;
 	}
 	
 	// Cohesion rule
-	// Steering to the average neighbors position
+	
 	cohesion(neighbors) {
 	  let steering = createVector();
 	  for (let other of neighbors) steering.add(other.pos); // Sum of neighbor positions
@@ -281,18 +234,14 @@ class Boid {
 	  
 	  
 		// VERSION WITHOUT QUADTREE
-		// Make an array of neighbors, i.e. all boids closer than the perception radius
-		// The array will be passed to the different flocking behaviors
+		
 		for (let other of boids) {
 		  let distance = this.pos.dist(other.pos);
 		  if (other != this && distance < radius) {
-			other.distance = distance; // Record the distance so it can be used later
-			neighbors.push(other); // Put this neighbor in the "neighbors" array
+			other.distance = distance; 
+			neighbors.push(other); 
 		  }
 		}
-	  
-	  
-  
 	  
 	  // Calculate the force of alignments and apply it to the boid
 	  let alignment = this.alignment(neighbors);
@@ -312,13 +261,12 @@ class Boid {
 	  separation.mult(mouseX/10);
 	  this.acc.add(separation);
 	  
-	  // If the boid is flies too high or too low, apply another force to make it fly around the middle of space's depth
 	  if (this.pos.z < -depth/8 || this.pos.z > depth/8) {
 		let force = createVector(0, 0, -this.pos.z / depth * this.maxForce * 2);
 		this.acc.add(force);
 	  }
 	  
-	  // If the boid has no neighbor, apply random forces so it can go find other boids
+
 	  if (neighbors.length == 0) {
 		let force = p5.Vector.random3D().mult(this.maxForce/4);
 		force.z = 0; // Only go find other in an XY plane
@@ -331,12 +279,10 @@ class Boid {
 	  // Apply physics
 	  this.pos.add(this.vel);
 	  this.vel.add(this.acc);
-	  this.vel.mult(0.999); // Some friction
+	  this.vel.mult(0.999); 
 	  this.vel.limit(this.maxSpeed);
 	  this.acc.mult(0);
 	  
-	  // Teleport to opposite side if the boid goes further than a side of space (X and Y axis)
-	  // Except for the Z axis, as there is already a force keeping the boid from getting too far
 	  if (this.pos.x > width/2 + gap) this.pos.x -= width + 1.7 * gap;
 	  if (this.pos.x < -(width/2 + gap)) this.pos.x += width + 1.7 * gap;
 	  if (this.pos.y > height/2 + gap) this.pos.y -= height + 1.7 * gap;
@@ -347,7 +293,7 @@ class Boid {
 	show() {
 	  noStroke();
 	  
-	  //ambientMaterial(this.r, this.g, this.b);
+
   
 	  push()
 	  translate(this.pos.x, this.pos.y, this.pos.z);
@@ -387,12 +333,7 @@ class Boid {
 	}
   }
 
-  // This file contains the QuadTree class
-// as well as the Cube classe used by the QuadTree
-
-// Cube --------------------------------------------------
-// A cube delimiting the volume of a quad tree
-// or the volume used for asking boids from a quad tree
+ 
 class Cube {
 	constructor(x, y, z, w, h, d) {
 	  this.x = x;
@@ -426,15 +367,13 @@ class Cube {
 	}
   }
   
-  // QUAD TREE --------------------------------------------------
-  // The quad tree stores points in a tree structure
-  // to minimize the cost of distance calculation
+ 
   class QuadTree {
 	constructor(boundary, capacity) {
-	  this.boundary = boundary; // cube giving the borders of the quad tree
-	  this.capacity = capacity; // Maximum amount of points that can be stored in the quad tree
-	  this.boids = []; // Array storing the boids in the quad tree
-	  this.divided = false; // True when the quad tree subdivides
+	  this.boundary = boundary; 
+	  this.capacity = capacity; 
+	  this.boids = []; 
+	  this.divided = false; 
 	}
   
 	// Insert a boid in the quad tree
@@ -444,20 +383,16 @@ class Cube {
 		return false;
 	  }
   
-	  // Add the boid at this layer or a deeper layer depending on capacity
+	 
 	  if (this.boids.length < this.capacity) {
-		// Add the point to this layer if there is still room for it
 		this.boids.push(boid);
 		return true;
 	  } else {
-		// Otherwise, subdivide to make room for the new boid
-		// Subdivision divides the quad tree area into 8 new children quad trees
+		
 		if (!this.divided) {
 		  this.subdivide();
 		}
   
-		// Add the boid to the relevant subdivision
-		// N = North, S = South, E = East, W = West, B = Bottom, T = Top
 		if (this.NWT.insert(boid)) {
 		  return true;
 		} else if (this.NET.insert(boid)) {
@@ -478,9 +413,8 @@ class Cube {
 	  }
 	}
   
-	// Subdivides the quad tree if it is at full capacity, creating 8 new children quad trees
 	subdivide() {
-	  this.divided = true; // Informs of the subdivision to only subdivide once
+	  this.divided = true; 
   
 	  let x = this.boundary.x;
 	  let y = this.boundary.y;
@@ -489,8 +423,6 @@ class Cube {
 	  let h = this.boundary.h / 2;
 	  let d = this.boundary.d / 2;
   
-	  // Creates the 8 children quad trees with the relevant positions and area
-	  // North West Top quad tree
 	  let NWTBoundary = new Cube(x - w, y - h, z - d, w, h, d);
 	  this.NWT = new QuadTree(NWTBoundary, this.capacity);
   
